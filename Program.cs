c97db4feb78000456470c1ee7cb3fefd51525545
nikhita <nikhitajamadar1998@gmail.com>
2022-02-22 09:50:46 +0530

    Add contact to address Book

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook.EnterDetails();
+           
+        }
+    }
+}
