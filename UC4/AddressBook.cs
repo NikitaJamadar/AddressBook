376ccc2867002bf4db444f7dac5712c5639b6388
nikhita <nikhitajamadar1998@gmail.com>
2022-02-22 12:31:37 +0530

    Delete contact in address book

---
+++ UC4/AddressBook.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC4
+{
+    class AddressBook
+    {
+        public static List<ContactDetail> Details = new List<ContactDetail>();
+
+        public static void EnterDetails()
+        {
+            ContactDetail contact = new ContactDetail();
+
+            Console.WriteLine("Enter your First Name");
+            contact.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Last Name");
+            contact.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Address");
+            contact.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter your City");
+            contact.City = Console.ReadLine();
+
+            Console.WriteLine("Enter your state");
+            contact.State = Console.ReadLine();
+
+            Console.WriteLine("Enter your ZipCode");
+            contact.ZipCode = Console.ReadLine();
+
+            Console.WriteLine("Enter your Phone Number");
+            contact.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter your Email ID");
+            contact.EmailId = Console.ReadLine();
+
+            Details.Add(contact);
+            Console.WriteLine("----------------------------------------------");
+            ViewDetails();
+            Console.WriteLine($"{contact.FirstName} contact Added Succesfully");
+        }
+
+        public static void ViewDetails()
+        {
+           
+                foreach (var items in Details)
+                {
+                    Console.WriteLine("First Name:" + items.FirstName);
+                    Console.WriteLine("Last Name:" + items.LastName);
+                    Console.WriteLine("Address:" + items.Address);
+                    Console.WriteLine("city:" + items.City);
+                    Console.WriteLine("State:" + items.State);
+                    Console.WriteLine("Zip Code:" + items.ZipCode);
+                    Console.WriteLine("Phone Number:" + items.PhoneNumber);
+                    Console.WriteLine("Email ID:" + items.EmailId);
+                }
+           
+        }
+        public static void DeleteContact()
+        {
+            if (Details.Count > 0)
+            {
+                Console.Write("Enter name of contact you want to delete: ");
+                string deleteName = Console.ReadLine();
+
+
+                foreach (var item in Details)
+                {
+                    if (deleteName.ToLower() == item.FirstName.ToLower())
+                    {
+                        Details.Remove(item);
+                        Console.WriteLine(deleteName + "'s Contact is successfully deleted.");
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(deleteName + " does not exist in Address Book.");
+                    }
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Address Book is empty.");
+            }
+        }
+    }
+}
---
+++ UC4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book program");
+            AddressBook.EnterDetails();
+            AddressBook.DeleteContact();
+
+        }
+    }
+}
