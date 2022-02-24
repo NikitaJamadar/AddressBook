161bd50deef5aadccccbda570474613396e3fe26
nikhita <nikhitajamadar1998@gmail.com>
2022-02-24 11:34:38 +0530

    Add multiple address book

--- UC6/AddressBookDictionary.cs
+++ UC6/AddressBookDictionary.cs
@@ -13,60 +13,51 @@ namespace UC6
         {
             Console.WriteLine("Enter how many Address Book you want to add");
             int book = Convert.ToInt32(Console.ReadLine());
-            for(int i = 0; i < book; i++)
+            for (int i = 0; i < book; i++)
             {
                 Console.WriteLine("Enter Address Book name");
                 string addressBookName = Console.ReadLine();
-                if (addressBook.ContainsKey(addressBookName))
+
+                Console.WriteLine("How many contacts you want to add in this address Book");
+                int noOfContacts = Convert.ToInt32(Console.ReadLine());
+                while (noOfContacts > 0)
                 {
-                    Console.WriteLine("name is already exist in address book ");
-                    
+
+                    ContactDetail contact = new ContactDetail();
+
+                    Console.WriteLine("Enter your First Name");
+                    contact.FirstName = Console.ReadLine();
+
+                    Console.WriteLine("Enter your Last Name");
+                    contact.LastName = Console.ReadLine();
+
+                    Console.WriteLine("Enter your Address");
+                    contact.Address = Console.ReadLine();
+
+                    Console.WriteLine("Enter your City");
+                    contact.City = Console.ReadLine();
+
+                    Console.WriteLine("Enter your state");
+                    contact.State = Console.ReadLine();
+
+                    Console.WriteLine("Enter your ZipCode");
+                    contact.ZipCode = Console.ReadLine();
+
+                    Console.WriteLine("Enter your Phone Number");
+                    contact.PhoneNumber = Console.ReadLine();
+
+                    Console.WriteLine("Enter your Email ID");
+                    contact.EmailId = Console.ReadLine();
+
+                    Details.Add(contact);
+                    Console.WriteLine("----------------------------------------------");
+                    ViewDetails();
+                    Console.WriteLine($"{contact.FirstName} contact Added Succesfully");
+                    addressBook.Add(addressBookName, Details);
+                    noOfContacts--;
                 }
-                else
-                { 
-
-                    Console.WriteLine("How many contacts you want to add in this address Book");
-                    int noOfContacts = Convert.ToInt32(Console.ReadLine());
-                    while (noOfContacts > 0)
-                    {
-                        ContactDetail contact = new ContactDetail();
-
-                        Console.WriteLine("Enter your First Name");
-                        contact.FirstName = Console.ReadLine();
-
-                        Console.WriteLine("Enter your Last Name");
-                        contact.LastName = Console.ReadLine();
-
-                        Console.WriteLine("Enter your Address");
-                        contact.Address = Console.ReadLine();
-
-                        Console.WriteLine("Enter your City");
-                        contact.City = Console.ReadLine();
-
-                        Console.WriteLine("Enter your state");
-                        contact.State = Console.ReadLine();
-
-                        Console.WriteLine("Enter your ZipCode");
-                        contact.ZipCode = Console.ReadLine();
-
-                        Console.WriteLine("Enter your Phone Number");
-                        contact.PhoneNumber = Console.ReadLine();
-
-                        Console.WriteLine("Enter your Email ID");
-                        contact.EmailId = Console.ReadLine();
-
-                        Details.Add(contact);
-                        Console.WriteLine("----------------------------------------------");
-                        ViewDetails();
-                        Console.WriteLine($"{contact.FirstName} contact Added Succesfully");
-                        addressBook.Add(addressBookName, Details);
-                        noOfContacts--;
-                    }
-                } 
             }
         }
-       
-
         public static void ViewDetails()
         {
             foreach (var items in Details)
@@ -83,3 +74,10 @@ namespace UC6
         }
     }
 }
+   
+    
+
+
+
+    
+
